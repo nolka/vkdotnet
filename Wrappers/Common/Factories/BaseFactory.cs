0a2e9f856e021d812541aba8b02754ae4806d9c1
nolka <i@xternalx.com>
2011-03-02 00:43:32 +0700

    fixing filesystem

---
+++ Wrappers/Common/Factories/BaseFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiCore
+{
+    public abstract class BaseFactory
+    {
+        public ApiManager Manager;
+
+        public BaseFactory(ApiManager manager)
+        {
+            this.Manager = manager;
+        }
+    }
+}
