932f5202d1cf0b5b5a260a949876baefa4ec80d4
SnoUweR <snouwer@gmail.com>
2013-09-28 21:17:23 +0800

    audio.setBroadcast and audio.getPopular methods were added, added "genre" entry, audio.search now working correctly, audioSortOrder was updated, relogin function was added to session manager

--- ApiCore/Wrappers/Audio/AudioFactory.cs
+++ ApiCore/Wrappers/Audio/AudioFactory.cs
@@ -32,6 +32,7 @@ namespace ApiCore.Audio
                 a.Id = XmlUtils.Int("aid");
                 a.OwnerId = XmlUtils.Int("owner_id");
                 a.Duration = XmlUtils.Int("duration");
+                a.Genre = XmlUtils.Int("genre");
                 a.Artist = XmlUtils.String("artist");
                 a.Title = XmlUtils.String("title");
                 a.Url = XmlUtils.String("url");
@@ -75,13 +76,51 @@ namespace ApiCore.Audio
             }
             return null;
         }
-
-        public List<AudioEntry> Search(string query, AudioSortOrder order, bool withLyrics, int? count, int? offset)
+        
+        /// <summary>
+        /// Возвращает список аудиозаписей в соответствии с заданным критерием поиска
+        /// </summary>
+        /// <param name="query">текст поискового запроса</param>
+        /// <param name="order">Вид сортировки</param>
+        /// <param name="autoComplete">Если этот параметр равен 1, возможные ошибки в поисковом запросе будут исправлены</param>
+        /// <param name="withLyrics">Если этот параметр равен 1, поиск будет производиться только по тем аудиозаписям, которые содержат тексты</param>
+        /// <param name="performerOnly">Если этот параметр равен 1, поиск будет осуществляться только по названию исполнителя</param>
+        /// <param name="count">количество аудиозаписей, информацию о которых необходимо вернуть</param>
+        /// <param name="offset">смещение, необходимое для выборки определенного подмножетсва аудиозаписей</param>
+        /// <returns></returns>
+        public List<AudioEntry> Search(string query, AudioSortOrder order, bool withLyrics, int? count, int? offset, bool autoComplete = false, bool performerOnly = false)
         {
             this.Manager.Method("audio.search");
             this.Manager.Params("q", query);
-            this.Manager.Params("sort", order);
+            this.Manager.Params("auto_complete", ((autoComplete) ? 1 : 0));
             this.Manager.Params("lyrics", ((withLyrics) ? 1 : 0));
+            this.Manager.Params("performer_only", ((performerOnly) ? 1 : 0));
+            this.Manager.Params("sort", (Int32)order);
+
+            if (offset != null)
+            {
+                this.Manager.Params("offset", offset);
+            }
+            if (count != null)
+            {
+                this.Manager.Params("count", count);
+            }
+
+
+            XmlNode result = this.Manager.Execute().GetResponseXml().FirstChild;
+
+            XmlUtils.UseNode(result);
+            this.searchSongsCount = XmlUtils.Int("count");
+            return this.buildList(result);
+
+        }
+
+        public List<AudioEntry> GetPopular(bool onlyEng, AudioGenre genre, int? offset, int? count)
+        {
+            this.Manager.Method("audio.getPopular");
+            this.Manager.Params("only_eng", ((onlyEng) ? 1 : 0));
+            this.Manager.Params("genre_id", (Int32)genre);
+
             if (count != null)
             {
                 this.Manager.Params("count", count);
@@ -91,13 +130,34 @@ namespace ApiCore.Audio
                 this.Manager.Params("offset", offset);
             }
 
-            XmlNode result = this.Manager.Execute().GetResponseXml();
+            XmlNode result = this.Manager.Execute().GetResponseXml().FirstChild;
 
             XmlUtils.UseNode(result);
-            this.searchSongsCount = XmlUtils.Int("count");
             return this.buildList(result);
-
         }
+
+        /// <summary>
+        /// Транслирует аудиозапись в статус пользователю или сообществу
+        /// </summary>
+        /// <param name="audio">идентификатор аудиозаписи, которая будет отображаться в статусе, в формате owner_id+audio_id</param>
+        /// <param name="target_ids">перечисленные через запятую идентификаторы сообществ и пользователя, которым будет транслироваться аудиозапись. Идентификаторы сообществ должны быть заданы в формате "-gid"</param>
+        /// <returns></returns>
+        public bool SetBroadcast(string audio, string target_ids = null)
+        {
+            this.Manager.Method("audio.setBroadcast", new object[] { "audio", audio, "target_ids", target_ids });
+
+            this.Manager.Params("audio", audio);
+
+            if (target_ids != null)
+            {
+                this.Manager.Params("target_ids", target_ids);
+            }
+
+            XmlUtils.UseNode(this.Manager.Execute().GetResponseXml());
+
+            return XmlUtils.BoolVal();
+        }
+
 
         public string GetUploadServer()
         {
